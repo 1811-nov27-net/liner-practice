1d067c7998d6ba8c0a7e49de8b62ac3a170b21ff
Matthew Liner <mattliner028@gmail.com>
2018-11-30 09:48:05 -0600

    11/30 9:48

---
+++ week1/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //exceptions are runetime errors that we can potentially handle
+            //exceptions are objects and defined by classes like anything else
+            try
+            {
+                //try goes around code that is expected to throw an exception
+                var x = 4;
+                var y = x / 0;
+
+                Console.WriteLine("never prints because an exception is thrown first");
+            }
+            catch (DivideByZeroException e)
+            {
+                //handle the exception in catch block
+                Console.WriteLine("divided by zero, moving on");
+                //at the end of catch, we move on with business
+            }
+
+            BadCode();
+            Console.WriteLine("the program continues");
+            
+        }
+
+        static void BadCode()
+        {
+
+            try
+            {
+                object o = true;
+                string s = (string)o;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("won't print because this isn't a div by zero!");
+            }
+
+            //we're not handling the specific exception, so it will propogate up...
+        }
+    }
+}
---
+++ week1/LINQAndTesting/LINQAndTesting.Library/MyCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQAndTesting.Library
+{
+    /// <summary>
+    /// a list with some extra helper methods 
+    /// </summary>
+    /// <remarks>
+    /// two strategies we could use to leverage the builtin collection classes:
+    /// - inheritance (MyCollection IS A List
+    /// - composition (MyCollection HAS A List
+    /// </remarks>
+    public class MyCollection
+    {
+        // "readonly" just means I can't reassign "_list" to a different object
+        // later. it doesn't mean I can't modify the object with its methods.
+        private readonly List<string> _list = new List<string>();
+
+        // ever class has at least one constructor
+        // if you don't define one, it has a default constructor without any parameters
+        //  "public MyCollection() {}"
+        // but, as soon as you define any constructor, that default on will not be added.
+
+        public void Sort()
+        {
+            _list.Sort();
+        }
+
+        public void Add(string item)
+        {
+            _list.Add(item);
+        }
+
+        // property without a "set"
+        // calling code can say "coll.Length" instead of coll.GetLength()
+        public int Length { get { return _list.Count;  } }
+
+        public string Get(int index)
+        {
+            return _list[index];
+        }
+
+        public string Longest()
+        {
+            int longestLength = 0;
+            string longest = null;
+            foreach (var item in _list)
+            {
+                if (item != null && item.Length > longestLength)
+                {
+                    longestLength = item.Length;
+                    longest = item;
+                }
+            }
+            return longest;
+        }
+    }
+}
